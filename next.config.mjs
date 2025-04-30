01d6b0bda85ab193590cf5ac62d7b9da190c1c95
SAKPA freeman <freesakpa@gmail.com>
2025-04-30 11:55:33 +0000

    ajout fonctionnalite mobile pa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import withPWA from '@ducanh2912/next-pwa'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // autres configurations Next.js...
+}
+
+export default withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: process.env.NODE_ENV === 'development',
+    workboxOptions: {
+      disableDevLogs: true,
+    },
+    // Exclusion recommandée pour Next.js
+    exclude: [
+      /middleware-manifest\.json$/,
+      /_middleware\.js$/,
+      /_middleware\.js\.map$/,
+      /middleware-runtime\.js$/,
+      /server\/pages-manifest\.json$/,
+      /chunks\/images\/.*$/ // Exclut les images optimisées
+    ]
+  }
+})
